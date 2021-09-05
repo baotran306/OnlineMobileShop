8e6d33391033302751d6ad045866324478751c13
Tran Quoc Bao <tranquocb306@gmail.com>
2021-09-05 11:22:47 +0700

    update register staff, customer and hash password

--- MainAPI/ConnectSQL.py
+++ MainAPI/ConnectSQL.py
@@ -1,4 +1,5 @@
 import pyodbc
+import MainAPI.ExtraFunction as ef
 
 connect = pyodbc.connect(
     "Driver={SQL Server Native Client 11.0};"
@@ -23,7 +24,7 @@ class SqlFunction:
                 SELECT @RC AS rc;
             """
             values = (first_name, last_name, gender, identity_card, email, phone_num,
-                      day_of_birth, address, username, password)
+                      day_of_birth, address, username, ef.hash_password(password))
             cursor.execute(sql, values)
             cursor.commit()
             return True
@@ -42,7 +43,7 @@ class SqlFunction:
                 SELECT @RC as rc;
             """
             values = (first_name, last_name, gender, identity_card, email, phone_num, day_of_birth, address,
-                      username, password, salary, role)
+                      username, ef.hash_password(password), salary, role)
             cursor.execute(sql, values)
             cursor.commit()
             return True
@@ -96,9 +97,9 @@ class SqlFunction:
         try:
             if self.check_existed_user(user, 1):
                 current_pass = self.get_current_staff_password(user)
-                if old_pass == current_pass:
+                if ef.hash_password(old_pass) == current_pass:
                     cursor = self.func
-                    cursor.execute('exec ChangePasswordStaff ?, ?', user, new_pass)
+                    cursor.execute('exec ChangePasswordStaff ?, ?', user, ef.hash_password(new_pass))
                     cursor.commit()
                     return True
                 else:
@@ -126,9 +127,9 @@ class SqlFunction:
         try:
             if self.check_existed_user(user, 0):
                 current_pass = self.get_current_customer_password(user)
-                if old_pass == current_pass:
+                if ef.hash_password(old_pass) == current_pass:
                     cursor = self.func
-                    cursor.execute('exec ChangePasswordCustomer ?, ?', user, new_pass)
+                    cursor.execute('exec ChangePasswordCustomer ?, ?', user, ef.hash_password(new_pass))
                     cursor.commit()
                     return True
                 else:
@@ -150,11 +151,59 @@ class SqlFunction:
             print(ex)
             return False
 
+    def get_list_customer(self):
+        try:
+            cursor = self.func
+            cursor.execute('select * from GetListCustomer()')
+            ans = []
+            for r in cursor:
+                tmp = [r[0], r[1], r[2], r[3], r[4], r[5], r[6], str(r[7]), r[8], r[9], r[10], r[11]]
+                ans.append(tmp)
+            cursor.commit()
+            return ans
+        except Exception as ex:
+            print(ex)
+            return []
+
+    def get_list_staff(self):
+        try:
+            cursor = self.func
+            cursor.execute('select * from GetListStaff()')
+            ans = []
+            for r in cursor:
+                tmp = [r[0], r[1], r[2], r[3], r[4], r[5], r[6], str(r[7]), r[8], r[9], r[10], r[11],
+                       r[12], float(r[13]), r[14]]
+                ans.append(tmp)
+            cursor.commit()
+            return ans
+        except Exception as ex:
+            print(ex)
+            return []
+
+    def get_list_phone(self):
+        try:
+            cursor = self.func
+            cursor.execute('select * from GetListPhone()')
+            ans = []
+            for r in cursor:
+                ans.append(r)
+            cursor.commit()
+            return ans
+        except Exception as ex:
+            print(ex)
+            return []
+
     # not complete
     def delete_staff(self, id_staff):
         try:
-            cursor = self.func
-            pass
+            staff_list = self.get_list_staff()
+            for staff in staff_list:
+                if id_staff in staff:
+                    cursor = self.func
+                    cursor.execute('exec DeleteStaff ?', id_staff)
+                    cursor.commit()
+                    return True
+            return False
         except Exception as ex:
             print(ex)
             return False
@@ -162,13 +211,15 @@ class SqlFunction:
 
 # Test Function
 sql_connect = SqlFunction()
-# print(sql_connect.insert_customer('Liên', 'Trần Bích', 'Nam', '123456789', 'lienlien111@gmail.com', '0111111111',
-#                                   '2004-04-09', 'Thái Bình', 'lienlien111', '123'))
+# print(sql_connect.insert_customer('Ngọc', 'Phạm Thị Bích', 'Nữ', '197584732', 'ngocngungoc@gmail.com', '0982312312',
+#                                   '2003-04-09', 'Thái Bình', 'ngocngoc', 'ngoc123'))
 # print(sql_connect.insert_staff('Trung', 'Nguyễn Đình', 'Nam', '1010101010', 'trungnd224@gmail.com', '0123456888',
 #                                '1980-03-21', 'Hà Tây', 'trungnd224', '123', 1000000, 3))
 # print(sql_connect.get_current_staff_password('tramskt1'))
 # print(sql_connect.test())
 # print(sql_connect.check_existed_user('baobao', 0))
-# print(sql_connect.change_password_customer('mumumu', '12233', 'abc1'))
+# print(sql_connect.change_password_customer('ngocngoc', 'ngoc123', 'abc1'))
 # print(sql_connect.change_password_staff('tramskt1', '123', '456'))
 # print(sql_connect.change_password_staff('hoaihoai1', '1234', '456'))
+# print(sql_connect.get_list_customer())
+print(sql_connect.delete_staff('STAFF_1'))
---
+++ MainAPI/ExtraFunction.py
@@ -0,0 +1,39 @@
+import re
+import hashlib
+
+
+def hash_password(password):
+    my_key = 'guess_key'
+    db_password = password + my_key
+    new_pass = hashlib.md5(db_password.encode())
+    return new_pass.hexdigest()
+
+
+def check_regex_password(your_pass):
+    if " " in your_pass:
+        print("----False Regex Password(Type 1)----")
+        return False
+    punctuation = "!\"#$%&'()*+,-/:;<=>?[\\]^`{|}~"
+    for pun in punctuation:
+        if pun in your_pass:
+            print("----False Regex Password(Type 2)----")
+            return False
+    regex_type = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,20}$"
+    if re.search(regex_type, your_pass):
+        return True
+    print("----False Regex Password----")
+    return False
+
+
+def check_positive_number(num):
+    if num < 0:
+        print('----Deny negative number----')
+        return False
+    return True
+
+
+def check_str_all_num(your_str):
+    if your_str.isdigit():
+        return True
+    print('----Deny phone number or identity card contains character----')
+    return False
